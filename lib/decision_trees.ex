3fb42a41bc1c8c0cf53eea39ef50819565a90c42
Derek Duncan <mail-spam@derekduncan.me>
2016-08-06 02:34:34 -0500

    getting basic algoritm methods created

--- lib/decision_trees.ex
+++ lib/decision_trees.ex
@@ -1,38 +1,54 @@
 defmodule DecisionTrees do
 
   def seed do
-    list = [
+    items = [
       [body: "aluminum", weight: 2, color: "gray"],
       [body: "plastic", weight: 4, color: "black"],
-      [body: "plastic", weight: 2, color: "gray"]
+      [body: "plastic", weight: 2, color: "gray"],
+      [body: "aluminum", weight: 5, color: "red"],
+      [body: "plastic", weight: 1, color: "white"],
     ]
-    divide_list(list, :weight, 3)
   end
 
   @doc """
-  Divide a list into two children lists
+  Divide a items into two children lists
   """
-  def divide_list(list, split_key, split_value) do
-    # Divide the list into two segments
-    divided_list = list
+  def divide_items(items, split_key, split_value) do
+    # Divide the items into two segments
+    divided_list = items
     |> Enum.partition(&(decide_split(&1[split_key], split_value)))
-    elem(divided_list, 0)
   end
 
-  defp decide_split(value, split_value) when is_number(value) and is_number(split_value), do: value >= split_value
+  defp decide_split(value, split_value)
+  when is_number(value) and is_number(split_value) do
+    value >= split_value
+  end
+
   defp decide_split(value, split_value), do: value == split_value
 
   @doc """
   Gets the count of values for a specific key
   """
-  # def unique_counts(key) do
-  #   # Find unique counts
-  # end
+  def unique_counts([ head | tail], counts, unique_key) do
+    unique_value = head[unique_key]
+    old_value = Map.get(counts, unique_value, 0)
+    new_counts = Map.put(counts, unique_value, old_value + 1)
+
+    unique_counts(tail, new_counts, unique_key)
+  end
+  def unique_counts([], counts, _), do: counts
 
   @doc """
   Calculates the entropy of a list's values
   """
-  # def entropy(list) do
-  #   # Calculate entropy
-  # end
+  def entropy(items) do
+    # Calculate entropy
+    log2 = &(:math.log(&1) / :math.log(2))
+    results = unique_counts(items, %{}, :weight)
+
+    Enum.reduce(Map.keys(results), fn key, acc ->
+      p = results[key] / length(items)
+      acc - p * log2.(p)
+    end)
+  end
 end
