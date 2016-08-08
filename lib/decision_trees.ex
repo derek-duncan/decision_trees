aa0143db943c85af3b9bbd42de03219ddaeb3621
Derek Duncan <mail-spam@derekduncan.me>
2016-08-08 08:44:31 -0500

    progress

--- lib/decision_trees.ex
+++ lib/decision_trees.ex
@@ -10,44 +10,64 @@ defmodule DecisionTrees do
     ]
   end
 
-  @doc """
-  Divide a items into two children lists
+  @doc ~S"""
+  Divide items into two children lists
+
+  ## Examples
+
+    iex> items = [
+    ...> [body: "aluminum", weight: 2, color: "gray"],
+    ...> [body: "plastic", weight: 4, color: "black"],
+    ...> [body: "plastic", weight: 2, color: "gray"],
+    ...> [body: "aluminum", weight: 5, color: "red"]
+    ...> ]
+    iex> DecisionTrees.divide_items(items, :body, "aluminum")
+    {
+      [
+        [body: "aluminum", weight: 2, color: "gray"],
+        [body: "aluminum", weight: 5, color: "red"]
+      ],
+      [
+        [body: "plastic", weight: 4, color: "black"],
+        [body: "plastic", weight: 2, color: "gray"]
+      ]
+    }
   """
   def divide_items(items, split_key, split_value) do
-    # Divide the items into two segments
-    divided_list = items
-    |> Enum.partition(&(decide_split(&1[split_key], split_value)))
+    Enum.partition(items, &(decide_split(&1[split_key], split_value)))
   end
 
-  defp decide_split(value, split_value)
-  when is_number(value) and is_number(split_value) do
-    value >= split_value
-  end
-
+  defp decide_split(value, split_value) when is_number(value) and is_number(split_value), do: value >= split_value
   defp decide_split(value, split_value), do: value == split_value
 
   @doc """
-  Gets the count of values for a specific key
+  Count the values of a specific key in a list of items
   """
-  def unique_counts([ head | tail], counts, unique_key) do
-    unique_value = head[unique_key]
-    old_value = Map.get(counts, unique_value, 0)
-    new_counts = Map.put(counts, unique_value, old_value + 1)
+  def unique_counts(items, unique_key),
+    do: do_unique_counts(items, %{}, unique_key)
 
-    unique_counts(tail, new_counts, unique_key)
+  defp do_unique_counts([ head | tail], counts, unique_key) do
+    unique_value_key = head[unique_key]
+    old_value = Map.get(counts, unique_value_key, 0)
+    new_counts = Map.put(counts, unique_value_key, old_value + 1)
+    do_unique_counts(tail, new_counts, unique_key)
   end
-  def unique_counts([], counts, _), do: counts
+
+  defp do_unique_counts([], counts, _), do: counts
 
   @doc """
   Calculates the entropy of a list's values
   """
-  def entropy(items) do
-    # Calculate entropy
-    log2 = &(:math.log(&1) / :math.log(2))
-    results = unique_counts(items, %{}, :weight)
+  def entropy(items, unique_key) do
+    unique_counts(items, unique_key) |> do_entropy(length(items))
+  end
 
-    Enum.reduce(Map.keys(results), fn key, acc ->
-      p = results[key] / length(items)
+  # Calculate entropy
+  @spec do_entropy(number, number) :: number
+  defp do_entropy(counts, len) do
+    log2 = &(:math.log(&1) / :math.log(2))
+    Enum.reduce(Map.keys(counts), 0, fn key, acc ->
+      p = counts[key] / len
       acc - p * log2.(p)
     end)
   end
--- lib/decision_trees/build_tree.ex
+++ lib/decision_trees/build_tree.ex
@@ -2,6 +2,58 @@ defmodule DecisionTrees.BuildTree do
   @moduledoc """
   Builds the decision tree
   """
-  # def build_tree(list) do
-  # end
+
+  alias DecisionTrees.BuildTree, as: BuildTree
+
+  defstruct key: nil, value: nil, results: nil, truthy_tree: nil, falsy_tree: nil
+
+  @doc """
+  Build a tree with the following steps
+  - Loop each key except the target key and generate the list of all possible different values in the considered column
+  """
+  def build_tree([]), do: %BuildTree{}
+  def build_tree(items) do
+    current_entropy_score = DecisionTrees.entropy(items, :weight)
+
+    # key_values = %{
+    #   :body => %{"aluminum" => 1, "plastic" => 1},
+    #   :weight => %{2 => 1, 5 => 1}
+    # }
+    keys = Keyword.keys(hd(items)) # Get the first item's keys as a template
+    key_values = Enum.reduce(keys, %{}, fn key, values ->
+      Map.put(values, key, Enum.reduce(items, %{}, fn item, values ->
+        Map.put(values, item[key], 1)
+      end))
+    end)
+
+    best_results = Enum.reduce key_values, %{}, fn {key, value}, _acc ->
+      Enum.reduce value, %{best_gain: 0, best_criteria: [], best_sets: []}, fn {value_key, _}, best ->
+        {set1, set2} = DecisionTrees.divide_items items, key, value_key
+
+        # Information gain
+        p = length(set1) / length(items)
+        gain = current_entropy_score - p * DecisionTrees.entropy(set1, :weight) - (1 - p) * DecisionTrees.entropy(set2, :weight)
+        if gain > best[:best_gain] and length(set1) > 0 and length(set2) > 0 do
+          best = best
+          |> Map.put(:best_gain, gain)
+          |> Map.put(:best_criteria, [key, value_key])
+          |> Map.put(:best_sets, [set1, set2])
+        end
+        best
+      end
+    end
+
+    if best_results[:best_gain] > 0 do
+      %BuildTree{
+        key: List.first(best_results[:best_criteria]),
+        value: List.last(best_results[:best_criteria]),
+        truthy_tree: build_tree(List.first(best_results[:best_sets])),
+        falsy_tree: build_tree(List.last(best_results[:best_sets])),
+      }
+    else
+      %BuildTree{
+        results: DecisionTrees.unique_counts(items, :weight)
+      }
+    end
+  end
 end
