64e897b9199b8d5c29b308c583a932ae9e7d0fc1
Derek Duncan <mail-spam@derekduncan.me>
2016-08-09 15:23:51 -0500

    beginning different data structure

--- lib/decision_trees.ex
+++ lib/decision_trees.ex
@@ -1,43 +1,7 @@
 defmodule DecisionTrees do
   def seed do
-    [
-      [
-        father_figure: true,
-        college: true,
-        income_level: 1,
-        gender: "male",
-      ],
-      [
-        father_figure: false,
-        college: false,
-        income_level: 3,
-        gender: "female",
-      ],
-      [
-        father_figure: true,
-        college: true,
-        income_level: 1,
-        gender: "female",
-      ],
-      [
-        father_figure: false,
-        college: false,
-        income_level: 2,
-        gender: "male",
-      ],
-      [
-        father_figure: false,
-        college: true,
-        income_level: 2,
-        gender: "female",
-      ],
-      [
-        father_figure: true,
-        college: false,
-        income_level: 3,
-        gender: "male",
-      ],
-    ]
+    {:ok, data} = File.read("./lib/sample_dataset.json")
+    Poison.decode!(data)
   end
 
   defdelegate build(dataset, target_key), to: DecisionTrees.Tree
